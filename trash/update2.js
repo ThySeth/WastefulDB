ab216793b02f843cd8eedde2c9798637ba45965d
Seth <58237855+ThySeth@users.noreply.github.com>
2021-02-01 12:46:34 -0600

    Updated methods

--- trash/update2.js
+++ trash/update2.js
@@ -1,28 +1,69 @@
-update(queries) { // update({id: "001", element: "money", change: -10})
-    queries.math = true;
-     if(queries.math != true || false) return console.error(`"math" element is boolean only.`);
-     if(!(queries.id || element || change)) return console.error(`"id", "element", or "change" is missing data.`);
-     let update = 0;
-    fs.readFile(`./data/${queries.id}.json`, (error, data) => {
-      if(error) throw error;
-       data = JSON.parse(data); data = data.table[0];
-            console.log(data);
-            foo.push(data);
-        update = foo.indexOf(foo[queries.element]);
-            console.log(update);
-        if(queries.math == true) {
-         data[update] = data[update] + queries.element
-          console.log(data);
-        } else {
-         data[update] = queries.change;
-        }
-        update = foo.push(data); update = JSON.stringify(update);
-        fs.writeFile(`./data/${queries.id}.json`, update, (err) => {
-          if(err) throw err;
-           if(this.feedback == true) {
-            console.log("Updated 1 document.");
-           }
-        })
+let object = {
+  table: []
+}
+
+update(foo) { // foo.id, foo.element, foo.change, foo.math
+  let element = foo.element; let change = foo.change;
+   if(!(foo.id || element || change)) return console.error("One or more variables missing."); if(math != true || false) return console.error("Variable 'math' is boolean only.");
+    
+    fs.readFile(`./node_modules/wastefuldb/data/${foo.id}.json`, (err, data) => {
+     if(err) throw err;
+      data = JSON.parse(data); data = data.table[0];
        
+       if(math == true) {
+        if(isNaN(data[element] || change)) return console.error("Variable 'element' or 'change' returned NaN.");
+          let num = data[element];
+        data[element] = num + change;
+       } else {
+        data[element] = change;
+       }
+        
+       object.table.push(data);
+        let jsoned = JSON.stringify(object);
+       fs.writeFile(`./node_modules/wastefuldb/data/${foo.id}.json`, jsoned, (error) => {
+          if(error) throw error;
+           if(this.feedback == true) {
+            console.log("Updated 1 document.");   
+           }
+       })
     })
-  }
+    
+}
+
+/*
+// Adapted from:
+
+const object = {
+  "table": [
+    {
+      "id": "Xv5312",
+      "name": "Seth",
+      "age": 22,
+      "money": 25,
+      "example": true
+    }
+  ]
+}
+
+const template = {
+    table: []
+}
+
+function edit(element, update) {
+
+let foo = JSON.stringify(object);
+foo = JSON.parse(foo);
+
+let bar = foo.table[0];
+
+console.log(bar);
+console.log(bar[element]);
+ bar[element] = update;
+ 
+ console.log(bar);
+  template.table.push(bar);
+  console.log(template);
+}
+
+edit("name", "Leo");
+*/
