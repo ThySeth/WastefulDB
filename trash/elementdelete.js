61b75697d7e46338e63f2205d8d1e733b8134561
Seth <58237855+ThySeth@users.noreply.github.com>
2021-09-13 11:29:24 -0500

    ! Test with full build (later) !

--- trash/elementdelete.js
+++ trash/elementdelete.js
@@ -1,30 +1,33 @@
-/**
-  @param {Object | String} data
-  
-  @param {Object} [element]
-  @param {String} [element.name]
-  @param {String} [element.content]
-*/
 
-delete(data, element={name: undefined, content: undefined) {
+    /**
+     * 
+     * @param {Object | String} [data] The identifier (aka name) of a file within the data file.
+     * @param {Object} [element]
+     * @argument {String} [element.name] The name of an element to search for and match the given "content".
+     * @argument {String | Number} [element.content] The content to match with the given "element".  
+     */
+
+    delete(data, element = {name:undefined, content:undefined}) {
         try {
-         let files, obj;
-          if(!(data instanceof Object) && element.name && element.content) {
-            files = fs.readdirSync(this.path); 
-              files.forEach(file => {
-                let target = fs.readFileSync(`${this.path}${file}`); target = JSON.parse(target); target = target[0];
-                 if(target[element.name] && target[element.name] == element.content) {
-                   fs.rm(`${this.path}${file}`); 
-                    this.feedback == true ? console.log("Successfully deleted 1 document.") : "";
-                 }
-              })
-          } else if((element.name > 0 && !element.content) || (!element.name && !element.content)){
-           return console.error("Missing field when searching via elements. ('element.name' or 'element.content' is missing."); 
-          } else {
-            fs.rm(`${this.path}${data.id || data}.json`);
-             this.feedback == true ? console.log("Successfully deleted 1 document.") : "";
-          }
-        } catch(err) {
-            console.log("Error: " + err.message);
+         if(!data && element.name != undefined && element.content != undefined) {
+            fs.readdir(`${this.path}`, (error, files => {
+              if(error) return console.log(`Error: ${error}`);
+                files.forEach(file => {
+                    let target = fs.readFileSync(file);
+                     target = JSON.parse(target); target = target[0];
+                      if(target[element.name] && target[element.name] == element.content) {
+                          fs.rm(`${this.path}${file}`);
+                           this.feedback == true ? console.log("Successfully deleted 1 document.") : ""
+                      }
+                })
+            }))
+         } else if(data || data.id) {
+             fs.rm(`${this.path}${data || data.id}`);
+              this.feedback == true ? console.log("Successfully deleted 1 document.") : ""
+         } else {
+             console.log("Missing variable(s). Provide an ID or an element 'name' and 'content'.");
+         }
+        }catch(err) {
+            console.log(`Error: ${err}`);
         }
     }
