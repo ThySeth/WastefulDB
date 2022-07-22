28d95eb6367e7afe0382615d0dd6f1633a080b51
Seth <58237855+ThySeth@users.noreply.github.com>
2022-07-21 21:49:40 -0500

    v1.5.1 - multiple updates arrive!
    + Added .mupdate()
       -- Update multiple objects 
    
    It took forever but it's done!

--- index.js
+++ index.js
@@ -225,6 +225,115 @@ class WastefulDB {
        }
   }
 
+  /**
+   * @param {String} id The identifier of the target document.
+   * @param {Array} data The array which stores objects to be sent to the target document.
+   * 
+   * @param {String} directory.dir The target directory of the document.
+   * 
+   * @ex > db.mupdate("1234", [ {key: "name", change: "Nick", math: false}, {key: "pass", change: "Password1234"} ]);
+   */
+
+  mupdate(id, data = [], directory={dir: this.path}) {
+    let obj, foo, bar;
+     try {
+       // CATCH MISSING ARGUMENTS
+        if(!id) return console.error("Unable to locate document due to missing identifier argument."); if(!(data instanceof Array) || data.length <= 1) return console.error("The data provided must be contained within an Array and have more than 1 Object.");
+        if(fs.existsSync(`${directory.dir}${id}.json`) == false) return console.log("Couldn't find any documents with the given identifier.");
+        // OPEN DOCUMENT
+          obj = fs.readFileSync(`${directory.dir}${id}.json`); obj = JSON.parse(obj);
+           // IF OBJ IS GREATER THAN 1 OBJECT <---
+            if(obj.length > 1) {
+              obj.forEach(item => {
+                // RUN THROUGH EACH DATA OBJECT IN EACH OBJECT ITEM <---
+                 data.forEach(thing => {
+                   if(!(thing instanceof Object)) return;
+                    if(!item[thing.key]) return;
+                     // FOUND DATA KEY IN OBJECT ITEM <---
+                      // IF DATA HAS CHILD
+                       if(thing.child) {
+                        // IF DATA INVOLVES MATH <---
+                         if(thing.math) {
+                          if(isNaN((item[thing.key])[thing.child]) || isNaN(thing.change)) return console.log("Given key or change returned NaN.");
+                           (item[thing.key])[thing.child] = (Number((item[thing.key])[thing.child])) + (Number(thing.change));
+                            (item[thing.key])[thing.child] = (item[thing.key])[thing.child] % 1 != 0 ? parseFloat(((item[thing.key])[thing.child]).toFixed(2)) : (item[thing.key])[thing.child];
+                             // CONTINUE ONWARD
+                         } else {
+                            // IF NO MATH BUT HAS CHILD <---
+                            (item[thing.key])[thing.child] = thing.change == "true" ? true : thing.change == "false" ? false : thing.change;
+                             thing.change == "undefined" ? delete (item[thing.key])[thing.child] : "";
+                             // CONTINUE ONWARD
+                         }
+                       } else {
+                        // IF DATA HAS NO CHILD <---
+                         // IF DATA INVOLVES MATH <---
+                         if(thing.math) {
+                           if(isNaN(item[thing.key]) || isNaN(thing.change)) return console.error("Given key or change returned NaN.");
+                            item[thing.key] = (Number(item[thing.key])) + (Number(thing.change));
+                             item[thing.key] = item[thing.key] % 1 != 0 ? (parseFloat((item[thing.key]).toFixed(2))) : item[thing.key];
+                              // CONTINUE ONWARD
+                         } else {
+                            // IF DATA HAS NO MATH <---
+                             item[thing.key] = (thing.change == "true" ? true : thing.change == "false" ? false : data.change);
+                              data.change == "undefined" ? delete (item[thing.key]) : "";
+                              // CONTINUE ONWARD
+                         }
+                       }
+                 })
+              })
+               bar = JSON.stringify(obj, null, 3);
+                fs.writeFileSync(`${directory.dir}${id}.json`, bar);
+                 this.feedback == true ? console.log("Successfully mupdated 1 bulk document.") : "";
+            } else {
+                // BASIC SINGLE OBJECT DOCUMENT <---
+                 obj = obj[0];
+                  data.forEach(item => {
+                    if(!obj[item.key]) return;
+                     // FOUND KEY IN OBJECT <---
+                      // IF DATA HAS CHILD <---
+                       if(item.child) {
+                         // IF DATA INVOLVES MATH <---
+                          if(item.math) {
+                            if(isNaN((obj[item.key])[item.child]) || isNaN(item.change)) return console.error("Given key or change returned NaN.");
+                              (obj[item.key])[item.child] = (Number((obj[item.key])[item.child])) + (Number(item.change));
+                               (obj[item.key])[item.child] = (obj[item.key])[item.child] % 1 != 0 ? parseFloat(((obj[item.key])[item.child]).toFixed(2)) : (obj[item.key])[item.child];
+                                // CONTINUE ONWARD
+                          } else {
+                            // IF DATA HAS NO MATH <---
+                             (obj[item.key])[item.child] = item.change == "true" ? true : item.change == "false" ? false : item.change;
+                              item.change == "undefined" ? delete (obj[item.key])[item.child] : "";
+                               // CONTINUE ONWARD
+                          }
+                       } else {
+                         // IF DATA HAS NO CHILD <---
+                          // IF DATA INVOLVES MATH <---
+                           if(item.math) {
+                             if(isNaN(obj[item.key]) || isNaN(item.change)) return console.error("Given key or change returned NaN.");
+                              obj[item.key] = (Number(obj[item.key])) + (Number(item.change));
+                               obj[item.key] = obj[item.key] % 1 != 0 ? parseFloat((obj[item.key]).toFixed(2)) : obj[item.key];
+                                // CONTINUE ONWARD
+                           } else {
+                            // IF DATA HAS NO MATH <---
+                             obj[item.key] = item.change == "true" ? true : item.change == "false" ? false : item.change;
+                              item.change == "undefined" ? delete obj[item.key] : "";
+                               // CONTINUE ONWARD
+                           }
+                       }
+                  })
+                obj = [ obj ];
+                 bar = JSON.stringify(obj, null, 3);
+                  fs.writeFileSync(`${directory.dir}${id}.json`, bar);
+                   this.feedback == true ? console.log("Successfully mupdated 1 document.") : "";
+            }
+     } catch(err) {
+        if(this.kill == true) {
+            throw new Error(err.message);
+        } else {
+            console.error("Error: " + err.message);
+        }
+     }
+  }
+
     /**
      * Reads each file within the directory and returns an array of objects with the information in each JSON file. Providing an id and/or key will filter through and push each file matching the profile.
      * @param {String} [data.id] The identifier stored within the file.
