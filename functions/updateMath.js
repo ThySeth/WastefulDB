eb0ab29aa063911487a2ba9382d83d708b8d3b3c
Seth <58237855+ThySeth@users.noreply.github.com>
2024-07-05 01:49:25 -0500

    Create updateMath.js

---
+++ functions/updateMath.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /**
+     * @param {Object} obj The object contained within the target document.
+     * @param {Object} data The arguments received in the update function.
+     */
+    objectMath: function(obj, data) {
+        // data = key, child, change
+        // obj = file
+        if(isNaN(data.change) || (isNaN(obj[data.key]) && (data.child ? isNaN((obj[data.key]))[data.child] : ""))) return -1
+    }
+}
