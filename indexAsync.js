9df54e2c77dc8ec8e45d8835eec4f8896a8724dc
Seth <58237855+ThySeth@users.noreply.github.com>
2024-07-12 20:58:41 -0500

    v1.0.3 - .update() done!
    + Added .update()
    + Added basic.js for my basic functions
    
    - File existence checks are unnecessary; try/catch error handling seems to work fine. ( remind me to change .insert() )
    
    ~ Hardcoded error messages now "throw new Error" rather than returning to stderr

--- indexAsync.js
+++ indexAsync.js
@@ -1,6 +1,6 @@
 const {fileReal} = require("./functions/existsAsync.js");
 const {kill, trailingSlash} = require("./functions/errorHandler.js");
-const {BNS} = require("./functions/basic.js")
+const {BNS, clog} = require("./functions/basic.js")
 
 const fs = require("fs");
 
@@ -26,8 +26,8 @@ class AsyncWastefulDB {
      * @returns {Array}
      */
     async insert(data, directory = {dir: this.path}) {
-      if(!(data instanceof Object)) return console.error("[.insert] : The data given must be contained within an Object.");
-      if(!data.id) return console.error("[.insert] : A document identifier must be provided before it can be created!");
+      if(!(data instanceof Object)) throw new Error("The data given must be contained within an Object.");
+      if(!data.id) throw new Error("A document identifier must be provided before it can be created!");
       try {
         directory.dir = trailingSlash(directory.dir);
         let real = await fileReal(`${directory.dir}${data.id}.json`);
@@ -60,28 +60,48 @@ class AsyncWastefulDB {
       }
     }
 
+    /**
+     * @param {String} identifier The identifier of the target document.
+     * 
+     * @param   {String} data.key The target key to be changed in the document.
+     * @param     {String} data.child The child key of the target key to change.
+     * @param   {String | Number} data.change What should the target key/child be changed to?
+     * @param   {Number} data.math Does the 'change' require *simple* math? (default: `false`)
+     * 
+     * @param {String} directory.dir The directory to search for the document in.
+     * @returns {Array}
+     */
     async update(identifier, data = {key, child, change, math: false}, directory = {dir: this.path}) {
       try {
-        if(!(data instanceof Object) || !data.key || !data.change) throw new Error("[.update] : The 'data' argument must be an Object containing at least both 'key' and 'change' keys with values.");
-         if(!identifier) throw new Error("[.update] : You have to provide the identifier of the target document.");
-          var doc = await fs.promises.open(`${trailingSlash(directory)}${identifier}.json`, "r+"); // flag "w+" for read and write
-            let content = await doc.readFile("utf8"); content = content[0];
-
-          if(!content[data.key]) throw new Error(`[.update] : The key provided, "${data.key}", couldn't be found in the target document.`);
+        if(!(data instanceof Object) || !data.key || !data.change) throw new Error("The 'data' argument must be an Object containing at least both 'key' and 'change' keys with values.");
+         if(!identifier) throw new Error("You have to provide the identifier of the target document.");
+          var doc = await fs.promises.open(`${trailingSlash(directory.dir)}${identifier}.json`, "r+"); // flag "w+" for read and write
+            let content = await doc.readFile("utf8"); content = (JSON.parse(content))[0];
+          // if(!content[data.key]) throw new Error(`The key provided, "${data.key}", couldn't be found in the target document.`);
             if(data.child) {
-              if(!(content[data.key])[data.child]) throw new Error(`[.update] : The child key "${data.child}" couldn't be found in the parent key "${data.key}".`);
+              if(!(content[data.key])[data.child]) throw new Error(`The child key "${data.child}" couldn't be found in the parent key "${data.key}".`);
                 if(data.change == "undefined") { // undefined has to be a string, otherwise the process takes it literally
                   delete (content[data.key])[data.child];
                 } else {
-                  if((data.math && isNaN((content[data.key])[data.child])) || (data.math && isNaN(data.change))) return console.error("[.update] : Unable to perform a math operation. Either the 'change' provided or the given key/child returned NaN.");
-                   (content[data.key])[data.child] = (data.math ? )
+                  if((data.math && isNaN((content[data.key])[data.child])) || (data.math && isNaN(data.change))) throw new Error("[.update] : Unable to perform a math operation. Either the 'change' provided or the given key/child returned NaN.");
+                   (content[data.key])[data.child] = (data.math ? (content[data.key])[data.child] + (data.change) : BNS(data.change)); // math? content + change : change
                 }
             } else { // No child key
-
+              if(data.change == "undefined") {
+                delete content[data.key];
+              } else {
+                if((data.math && isNaN(content[data.key])) || (data.math && isNaN(data.change))) throw new Error("Unable to perform a math operation. Either the 'change' provided or the given key returned NaN.");
+                 content[data.key] = (data.math ? (Number(content[data.key]) + Number(data.change)) : BNS(data.change));
+              }
             }
+            content = JSON.stringify([content]);
+            await fs.promises.writeFile("./syncData/4321.json", content);
+            this.feedback ? clog(`[.update] : Successfully updated document "${identifier}.json".`) : "";
+            return JSON.parse(content);
       } catch(err) {
-        await doc.close();
         kill(this.kill, err.message, ".update()");
+      } finally {
+        await doc?.close();
       }
     }
 
