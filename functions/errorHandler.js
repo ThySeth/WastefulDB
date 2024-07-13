809bde649f57df3a215d143143303ee28d166853
Seth <58237855+ThySeth@users.noreply.github.com>
2024-07-12 22:04:33 -0500

    Update errorHandler.js
    + trailingSlash() added to put a slash at the end of provided directories

--- functions/errorHandler.js
+++ functions/errorHandler.js
@@ -2,7 +2,7 @@ module.exports = {
     /**
      * @param {Boolean} allowed Is the process allowed to be killed?
      * @param {String} error The 'err.message'
-     * @param {*} fn The function name. ex: ".find()"
+     * @param {String} fn The function name. ex: ".find()"
      */
     kill: function(allowed = false, error, fn) {
         error = !error ? "(No error message provided)" : error;
@@ -11,5 +11,18 @@ module.exports = {
         } else {
             console.log(`An error occurred while executing [${fn}] : ${error}`);
         }
+    },
+
+    /**
+     * Throws a slash at the end of a directory; this'll make directory specifying less sensitive.
+     * 
+     * Also less bulky putting this here instead of in the main file.
+     * 
+     * @param {String} directory 
+     * @returns {String}
+     */
+    trailingSlash: function(directory) {
+        if(directory[directory.length-1] == "/") return directory;
+         return directory+"/";
     }
 }
